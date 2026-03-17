ad4b8961bc8ce35bf9b1a8005a0909154b99bad9
audibmwmerc-eng <audibmwmerc@gmail.com>
2026-03-17 09:43:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,6 +2,6 @@ api_id = 22382901 #change_with_your_Api_id.
 api_hash = "42286789c0c566a04c9f2f187daad5e1" #change_with_your_Api_hash.
 bot_token = "8601678200:AAFNbpQ9cfnIgSwdOHlAapbruN8-Uwf_61A" #change_with_your_bot_token.
 auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
+sudo_user = 1832467019 #change_with_your_user_id.
 log_channel = -1002154281532 #change_with_your_channel_id.
 txt_channel = -1002154281532 #change_with_your_channel_id.
