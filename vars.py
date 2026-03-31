203824338f1360f334681638770cc2b4e258674a
audibmwmerc-eng <audibmwmerc@gmail.com>
2026-03-31 12:30:23 +0530

    Secure vars.py with secrets

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
-api_id = 22382901 #change_with_your_Api_id.
-api_hash = "42286789c0c566a04c9f2f187daad5e1" #change_with_your_Api_hash.
-bot_token = "8601678200:AAFNbpQ9cfnIgSwdOHlAapbruN8-Uwf_61A" #change_with_your_bot_token.
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 auth_users = [999741495] #change_with_your_user_id.
 sudo_user = 1832467019 #change_with_your_user_id.
 log_channel = -1002154281532 #change_with_your_channel_id.
